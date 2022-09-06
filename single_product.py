b9d99be80408709ffbb4f6957910f1db09af774f
Olivier Meyer <looolive@gmail.com>
2022-09-06 10:56:20 +0200

    export single product to csv

--- single_product.py
+++ single_product.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
 
 url = 'http://books.toscrape.com/catalogue/dune-dune-1_151/index.html'
 response = requests.get(url)
@@ -38,7 +39,7 @@ def number_available():
     get the availability and extracts the number of books in stock from the string
     :return: string
     """
-    availability = soup.find("p", class_="availability").text
+    availability = product_info()['Availability']
     try:
         nb_available = re.search(r'(\d)', availability).group(0)
     except AttributeError:
@@ -48,14 +49,31 @@ def number_available():
 
 title = soup.find('h1').text
 product_description = soup.find("div", id="product_description").find_next_siblings("p")[0].text
-category = soup.find("li",  class_="active").find_previous_sibling().text
+category = soup.find("li",  class_="active").find_previous_sibling().text.strip()
 
 image_url = soup.find("div", id="product_gallery").find('img')['src']
 image_url = "http://books.toscrape.com" + image_url[5:]  # concat domain name + uri
 
 
+def single_product_to_csv():
+    with open('data/product.csv', mode='w', newline='') as csv_file:
+        fieldnames = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available',
+                      'product_description', 'category', 'review_rating', 'image_url']
+        writer = csv.DictWriter(csv_file, delimiter=';', quotechar='"', fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'product_page_url': url,
+                         'universal_ product_code (upc)': product_info()['UPC'],
+                         'title': title,
+                         'price_including_tax': product_info()['Price (incl. tax)'],
+                         'price_excluding_tax': product_info()['Price (excl. tax)'],
+                         'number_available': number_available(),
+                         'product_description': product_description,
+                         'category': category,
+                         'review_rating': review_rating(),
+                         'image_url': image_url})
 
 
+single_product_to_csv()
 
 
 
